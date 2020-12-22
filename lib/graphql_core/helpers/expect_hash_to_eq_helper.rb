f733150d4e574c0a70c9692b1ebea9575d888092
davidlarochelle <dlarochelle@fungo.ca>
2020-12-22 13:25:39 -0500

    fix expect array

--- lib/graphql_core/helpers/expect_hash_to_eq_helper.rb
+++ lib/graphql_core/helpers/expect_hash_to_eq_helper.rb
@@ -19,13 +19,17 @@ module GraphqlCore
 
     def expect_array_to_eq(expected, result)
       expected.each_with_index do |expect, index|
-        expect.each do |key, value|
-          if value.is_a?(Hash)
-            expect_hashes_to_eq(value, result[index][key])
-          else
-            pp key
-            expect(result[index][key]).to eq(value)
+        if expect.is_a?(Hash)
+          expect.each do |key, value|
+            if value.is_a?(Hash)
+              expect_hashes_to_eq(value, result[index][key])
+            else
+              pp key
+              expect(result[index][key]).to eq(value)
+            end
           end
+        else
+          expect(expect).to eq(result[index])
         end
       end
     end
